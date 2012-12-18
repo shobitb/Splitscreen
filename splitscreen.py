b289aec44180813f54dc8c72609e5a274de71084
Shobit Beltangdy <ssb402@nyu.edu>
2012-12-18 07:33:02 -0500

    Plays YouTube and S3 videos from CloudFront and JWPlayer

--- splitscreen.py
+++ splitscreen.py
@@ -56,23 +56,24 @@ def create_url():
 	url_id = ''.join(random.choice(chars) for x in range(size))
 	return jsonify({'url': url_id})
 
+""""
 @app.route('/url_movie_map')
 def url_movie_mapper():
 	page_url = request.args.get('url')
 	movie = request.args.get('movie')
-	connection = S3Connection(config.s3_key,config.s3_secret)
+	connection = S3Connection(con0fig.s3_key,config.s3_secret)
 	bucket = connection.get_bucket(bucket_name)
 	key = bucket.get_key(movie+'.mp4')
 	movie_url = key.generate_url(18000) # url is available for 18000 seconds i.e. 5 hours
 	url_movie_mapping[page_url] = movie_url
 	return jsonify({'status': 'success'})
-
+"""
 
 @app.route('/url_map')
 def url_map():
 	page_url = request.args.get('url')
 	movie = request.args.get('movie')
-	movie_url = 'http://d1sa8x6tbr5q03.cloudfront.net/'+movie+'.mp4'
+	movie_url = 'http://d8gfr27j5890z.cloudfront.net/'+movie+'.mp4'
 	url_movie_mapping[page_url] = movie_url
 	return jsonify({'status': 'success'})
 
@@ -83,7 +84,6 @@ def youtube_url_map():
 	movie_url = movie
 	url_movie_mapping[page_url] = movie_url
 	return jsonify({'status': 'success'})
-
 
 @app.route('/pusher/presence_auth',methods=['POST'])
 def auth():
